f3f3f6345e03ec6f04e9109c37d1ea9a11b580c1
Alok <aloksarkarxxxx@gmail.com>
2020-08-07 14:40:01 +0530

    empty input on folder path now defaults to current directory And header file now gets created on the working folder

--- header creator.py
+++ header creator.py
@@ -1,7 +1,6 @@
-folder=input("Enter folder path: ")
 def parser(target_file,destination_file):
     fh = open(target_file)
-    ds = open(destination_file,"a")
+    ds = open(destination_file,"a+")
     for line in fh:
         line=line.strip()
         if line.startswith("#"):
@@ -10,12 +9,20 @@ def parser(target_file,destination_file):
             elif line.endswith(">"):
                 ds.write(line[line.find('<')+1:-1]+"\n") #selects till end of line
 
+import os
 import glob
-fileList = [f for f in glob.glob(folder+"\\*.c")] + [f for f in glob.glob(folder+"\\*.cpp")]
+folder_path=input("Enter folder path: ")
+
+if(len(folder_path)<1):
+    folder_path=os.getcwd()
+#print(folder_path)
+
+fileList = [f for f in glob.glob(folder_path+"\\*.c")] + [f for f in glob.glob(folder_path+"\\*.cpp")]
+
 if not fileList:
     print("No files found!!")
     quit()
 for file in fileList:
     print()
-    parser(file,"headers.h")
+    parser(file,folder_path+"\\headers.h")
 print("Creation sucessfull..")
