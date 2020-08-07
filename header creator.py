b1ac2ce6caa91e6fff9dbfc87125f897f9e6d782
Alok <aloksarkarxxxx@gmail.com>
2020-08-07 13:43:22 +0530

    can perform in specified folder path

--- header creator.py
+++ header creator.py
@@ -1,23 +1,19 @@
-#folder=input("Enter folder path: ")
-def parser(x,y):
-    fh = open(x)
-    ds = open(y,"a")
+folder=input("Enter folder path: ")
+def parser(target_file,destination_file):
+    fh = open(target_file)
+    ds = open(destination_file,"a")
     for line in fh:
         line=line.strip()
         if line.startswith("#"):
             if line.endswith("\""):
-                ds.write(line[line.find('\"')+1:line.find("\"",line.find("\"")+1)]+"\n")#selects till the '\"' charcter
+                ds.write(line[line.find('\"')+1:line.find("\"",line.find("\"")+1)]+"\n")#selects till the ' " ' charcter
             elif line.endswith(">"):
                 ds.write(line[line.find('<')+1:-1]+"\n") #selects till end of line
 
 import glob
-fileList = [f for f in glob.glob("*.c")] + [f for f in glob.glob("*.cpp")]
+fileList = [f for f in glob.glob(folder+"\\*.c")] + [f for f in glob.glob(folder+"\\*.cpp")]
+
 for file in fileList:
     print()
     parser(file,"headers.h")
-
-# foi=[]
-# for file in files:
-#     if str(file).endswith(".c") or str(file).endswith(".cpp"):
-#         foi = foi +str (file)
-# print(foi)
+print("Creation sucessfull..")
