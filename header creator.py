f276dfccf34b275d708f28bdc969c520776e4ccc
Alok <aloksarkarxxxx@gmail.com>
2020-08-07 13:13:12 +0530

    header aggregator for current folder

---
+++ header creator.py
@@ -0,0 +1,23 @@
+#folder=input("Enter folder path: ")
+def parser(x,y):
+    fh = open(x)
+    ds = open(y,"a")
+    for line in fh:
+        line=line.strip()
+        if line.startswith("#"):
+            if line.endswith("\""):
+                ds.write(line[line.find('\"')+1:line.find("\"",line.find("\"")+1)]+"\n")#selects till the '\"' charcter
+            elif line.endswith(">"):
+                ds.write(line[line.find('<')+1:-1]+"\n") #selects till end of line
+
+import glob
+fileList = [f for f in glob.glob("*.c")] + [f for f in glob.glob("*.cpp")]
+for file in fileList:
+    print()
+    parser(file,"headers.h")
+
+# foi=[]
+# for file in files:
+#     if str(file).endswith(".c") or str(file).endswith(".cpp"):
+#         foi = foi +str (file)
+# print(foi)
